a22623f64c56063bbd0f6a8e8923ef74d2cbebcc
VladimirBalaur <vlad2balaur@gmail.com>
2020-04-07 19:44:54 +0300

    Added the functionality to the appointment form, fetched the countries from API, have set the doctors etc

--- js/appointmentForm.js
+++ js/appointmentForm.js
@@ -4,3 +4,147 @@ import { setCartItemsCount } from "./setCartItemsCount.js";
 
 const navOps = new Navbar();
 setCartItemsCount();
+
+class AppointmentFormUI {
+    //get data about doctors
+    async getData() {
+        try {
+            let responseDoctors = await fetch("../data/doctors.json", {
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+            });
+
+            let responseCountries = await fetch(
+                "https://restcountries-v1.p.rapidapi.com/all", {
+                    method: "GET",
+                    headers: {
+                        "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+                        "x-rapidapi-key": "f004cb53d4msh23a56366c5d1ab5p13f806jsn495f5fe7cbfa",
+                    },
+                }
+            );
+
+            if (responseDoctors.ok) {
+                this.services = await responseDoctors.json();
+            } else {
+                throw new Error("Failed to fetch data about doctors");
+            }
+
+            if (responseCountries.ok) {
+                this.countries = await responseCountries.json();
+            } else {
+                throw new Error("Failed to fetch data about countries");
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    constructor() {
+        this.getData().then(() => {
+            console.log(this.countries);
+
+            this.initializeSelectItems();
+            this.initializeCountries();
+        });
+    }
+
+    initializeSelectItems() {
+        let select = document.querySelector("select#reqDoctor");
+
+        this.services.forEach((service) => {
+            service.doctors.forEach((doctor) => {
+                let option = document.createElement("option");
+                option.setAttribute(
+                    "value",
+                    `${String(doctor.name).split(" ")[0]}-${
+            String(doctor.name).split(" ")[1]
+          }-${doctor.function}`
+                );
+
+                option.textContent = `${String(doctor.name)}, ${doctor.function} `;
+                select.appendChild(option);
+
+                option.dataset.serviceName = String(service.serviceName)
+                    .toLowerCase()
+                    .replace(/ /g, "-");
+            });
+        });
+        // console.log(this.doctors.doctors);
+    }
+
+    initializeCountries() {
+        let select = document.querySelector("select#originCountry");
+
+        this.countries.forEach((country) => {
+            let option = document.createElement("option");
+            option.value = country.name;
+            option.textContent = country.name;
+
+            select.appendChild(option);
+        });
+    }
+}
+
+const UI = new AppointmentFormUI();
+
+let select = document.querySelector("select#reqService");
+select.addEventListener("change", (event) => {
+    let serviceId = event.currentTarget.value;
+    let reqServiceDiv = document.querySelector(
+        `.left div[data-service="${serviceId}"]`
+    );
+    console.log(serviceId);
+
+    let otherServiceDivs = document.querySelectorAll(".left div[data-service]");
+
+    for (let div of otherServiceDivs) {
+        if (div !== reqServiceDiv) {
+            div.classList.remove("focused");
+            console.log(div);
+        }
+    }
+
+    let doctorOptions = document
+        .querySelectorAll("select#reqDoctor option")
+        .forEach((option) => {
+            if (
+                option.dataset.serviceName !== serviceId &&
+                option.getAttribute(disabled) == false
+            ) {
+                option.style.display = "none";
+            } else {
+                option.style.display = "block";
+            }
+        });
+
+    reqServiceDiv.classList.add("focused");
+});
+
+let serviceDivs = document.querySelectorAll(".left div[data-service]");
+serviceDivs.forEach((item) => {
+    item.addEventListener("click", (event) => {
+        //set the service in the input field, corresponding to the clicked div
+        let service = event.currentTarget.dataset.service;
+
+        let serviceSelect = document.querySelector("#reqService");
+        serviceSelect.value = service;
+
+        let reqOption = document.querySelector(
+            `#reqDoctor option[data-service-name="${service}"]`
+        );
+
+        //we filter the doctors that correspond to the clicked service
+        let doctorOptions = document
+            .querySelectorAll("select#reqDoctor option")
+            .forEach((option) => {
+                if (option.dataset.serviceName !== service) {
+                    option.style.display = "none";
+                } else {
+                    option.style.display = "block";
+                }
+            });
+    });
+});
