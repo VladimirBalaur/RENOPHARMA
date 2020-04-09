dfd68265a483935aa8f50c74863fa05454336bc7
VladimirBalaur <vlad2balaur@gmail.com>
2020-04-09 11:54:22 +0300

    started creating checkout page

---
+++ js/checkout.js
@@ -0,0 +1,6 @@
+import { Navbar } from "../components/navbar/script.js";
+import { setCartItemsCount } from "./setCartItemsCount.js";
+import openModal from "../components/modal/modal.js";
+const navUI = new Navbar();
+
+setCartItemsCount();
